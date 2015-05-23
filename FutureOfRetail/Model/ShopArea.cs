f8e3769540430213cdb5c92c6a2bb53ab2f3776c
ÿatanasov <yordan.ata@gmail.com>
2015-05-23 17:16:36 +0200

    changes

---
+++ FutureOfRetail/Model/ShopArea.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FutureOfRetail.Model
+{
+    public class ShopArea
+    {
+        public int Id { get; set; }
+        public int AreaId { get; set; }
+        public int ShopId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
