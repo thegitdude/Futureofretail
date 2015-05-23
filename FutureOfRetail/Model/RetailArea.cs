9db4b461776b31b3e98c6ad24a6af578d2787494
ÿatanasov <yordan.ata@gmail.com>
2015-05-23 17:41:47 +0200

    changes

--- FutureOfRetail/Model/RetailArea.cs
+++ FutureOfRetail/Model/RetailArea.cs
@@ -5,11 +5,10 @@ using System.Web;
 
 namespace FutureOfRetail.Model
 {
-    public class ShopArea
+    public class RetailArea
     {
         public int Id { get; set; }
         public int AreaId { get; set; }
-        public int ShopId { get; set; }
         public string Name { get; set; }
 
     }
---
+++ FutureOfRetail/Service/RetailAreaService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Dapper;
+using FutureOfRetail.Model;
+using FutureOfRetail.Tools;
+
+
+namespace FutureOfRetail.Service
+{
+    public class RetailAreaService
+    {
+        public static List<RetailArea> GetShopAreas()
+        {
+            List<RetailArea> areas = new List<RetailArea>();
+            try
+            {
+                using (var sqlCon = ConnectionClass.GetTrafficButtyCon())
+                {
+                    areas =
+                        sqlCon.Query<RetailArea>(String.Format(@"SELECT * from FROM [{0}].[dbo].[ShopArea]",
+                        ConnectionClass.sampleDatabaseName)).ToList();
+
+                    // Do something with the results:
+                    // Additional code here...
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return areas;
+        }
+
+    }
+}
