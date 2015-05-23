d96ad074210902035d5ec45298b0323248e610e4
catammd <catammd@gmail.com>
2015-05-23 23:06:59 +0200

    Ensure the right data is in the section dropdown

---
+++ FutureOfRetail/Scripts/Departments.js
@@ -0,0 +1,21 @@
+﻿/// <reference path = "jquery-1.8.2.js"/>
+/// <reference path = "knockout-3.3.0.js"/>
+
+var ViewModel = function () {
+    this.retailAreas = ko.observableArray();
+    $.ajax({
+        url: "/Default.aspx/GetRetailAreas",
+        contentType: "application/json; charset=utf-8",
+        type: "GET",
+        success: function (data) {
+            $.each(data.d, function (innerObj,index) {
+                retailAreas.push(index.Name);
+            });
+        },
+        error: function (data) {
+            alert("error occured");
+        }
+    });
+};
+ko.applyBindings(ViewModel);
+
