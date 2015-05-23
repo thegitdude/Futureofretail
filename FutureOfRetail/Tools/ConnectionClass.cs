83949734ad1f88eb1a652b5357825563110075a0
andrei <alloandrei@yahoo.com>
2015-05-23 15:31:02 +0200

    changed connection class credentials

--- FutureOfRetail/Tools/ConnectionClass.cs
+++ FutureOfRetail/Tools/ConnectionClass.cs
@@ -10,12 +10,8 @@ namespace FutureOfRetail.Tools
     public class ConnectionClass
     {
         private static SqlConnectionStringBuilder dbString;
-        private static readonly string userName = "mysite1";
-        private static readonly string password = "Zt4Tu3#<8Lyd";
-        private static readonly string dataSource = "mssql1.gear.host"; //database server, etc.
-        public static readonly string sampleDatabaseName = "ourlittlethings"; //i.e. "master", etc.
-        public static readonly string sampleDatabaseNameCloud = "mysite1"; //i.e. "master", etc.
-        private static string constr = @"Server=mssql1.gear.host;Database=retail;User Id=ourlittlethings;Password=Ym3c9!0#TA7H";
+       
+        private static string constr = @"Server=mssql1.gear.host;Database=retail;User Id=futureofretail;Password=Lf2b9iC#^y8A";
         private static string constrLocal = @"Server=ASUS\SQLEXPRESS;Database=retail;Trusted_Connection=Yes";
 
 
@@ -25,7 +21,7 @@ namespace FutureOfRetail.Tools
             {
                 // Execute the user defined function or stored procedure and fetch the
                 // matching results:
-                var conn = new SqlConnection(constrLocal);
+                var conn = new SqlConnection(constr);
                 conn.Open();
                 return conn;
             }
