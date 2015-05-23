1a82e618438df714e4accc6647f009b954954be5
andrei <alloandrei@yahoo.com>
2015-05-23 17:43:07 +0200

    added web method to default

--- FutureOfRetail/Default.aspx.cs
+++ FutureOfRetail/Default.aspx.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FutureOfRetail.Model;
+using FutureOfRetail.Service;
 
 namespace FutureOfRetail
 {
@@ -13,5 +17,14 @@ namespace FutureOfRetail
         {
 
         }
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet = true)]
+        public static List<RetailArea> GetRetailAreas(int id)
+        {
+            //html encode
+         
+        }
+
     }
 }
